1472cf2b12516e24b64c61668225ed37563ea084
Jirasin46033 <jirasin.46033@mail.kmutt.ac.th>
2021-04-14 16:07:29 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home" style="height: 38rem">
+    <div class="all">
+      <div class="header">My Friendships</div>
+      <router-link to="/welcome"> <button class="btn">Click here</button></router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {};
+</script>
+
+<style scoped>
+.header {
+  @apply text-white text-2xl font-semibold uppercase;
+}
+
+.btn {
+  @apply mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500;
+}
+
+.home {
+  @apply w-full bg-cover bg-center bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500;
+}
+
+.all {
+  @apply pt-60;
+}
+</style>
---
+++ src/views/Welcomepage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bg-welcome">
+    <div>
+      <div class="content-box">
+        <h3>Welcome!!!</h3>
+        <img src="../assets/SIT25Group.jpg" />
+        <p class="text-center leading-relaxed">This is My friendship.</p>
+        <span class="text-center">By Peck Jirasin</span>
+        <button
+          class="px-24 py-4 bg-gray-900 rounded-md text-white text-sm focus:border-transparent"
+        >
+          Write your friendship
+        </button>
+        <button
+          class="px-24 py-4 bg-gray-900 rounded-md text-white text-sm focus:border-transparent"
+        >
+          Read another friendship
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.bg-welcome {
+  @apply min-h-screen min-w-full bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 flex items-center justify-center;
+}
+
+.content-box {
+  @apply flex flex-col max-w-md bg-white px-8 py-6 rounded-xl space-y-5 items-center;
+}
+
+h3{
+    @apply font-serif font-bold text-gray-900 text-xl
+}
+
+img{
+ @apply w-full rounded-md
+}
+</style>

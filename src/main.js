a204287f2ccb905e111c3e99d83f04db8c9ba0fc
Jirasin46033 <jirasin.46033@mail.kmutt.ac.th>
2021-04-18 14:17:04 +0700

    [62130500121][UPDATE]About.vue and everything [ADD]Navigate.vue

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import Navigate from './components/Navigate.vue'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+const app = createApp(App)
+app.component('Navigate',Navigate)
+app.use(router).mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="bg-about">
+    <div>
+      <div class="content-box">
+        <h3>About</h3>
+        <img src="../assets/Jirasin.jpg" />
+        <p>Hi My name is Jirasin Phaobthong</p>
+        <span>Nice to meet you!!</span>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.bg-about {
+  @apply min-h-screen min-w-full bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 flex items-center justify-center;
+}
+
+.content-box {
+  @apply flex flex-col max-w-md bg-white px-8 py-6 rounded-xl space-y-5 items-center;
+}
+
+h3 {
+  @apply font-serif font-bold text-gray-900 text-xl;
+}
+</style>
